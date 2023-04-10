c346312c4680378020efae649655aca0edab77ec
Raf4ok0 <119265604+Raf4ok0@users.noreply.github.com>
2023-04-10 11:01:37 +0300

    Update UserService.java

--- src/main/java/ru/yandex/practicum/filmorate/Service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/Service/UserService.java
@@ -8,7 +8,6 @@ import ru.yandex.practicum.filmorate.Storage.UserStorage;
 import ru.yandex.practicum.filmorate.Validators.UserValidator;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
